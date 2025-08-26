1c1a03d85391e852faffeaa1cfbd58d17830478a
Rashik Nagarajan <98273879+N-Rashik@users.noreply.github.com>
2025-08-26 08:47:02 +0530

    Delete WorkNest directory

--- WorkNest/WorkNestApp/worknestdb.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS worknestdb;
-USE worknestdb;
--- Tables are auto-created/updated by Hibernate (hbm2ddl=update).
--- Ensure MySQL user has privileges and server is running.
